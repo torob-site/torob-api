e639fa17ffd52ce52e299debd4ec836963ee35d1
A9H8J9 <amirhoseinjabbarzade36@gmail.com>
2026-07-05 19:32:01 +0330

    feat: add similar product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('products')
@@ -6,7 +13,13 @@ export class ProductController {
   constructor(private productService: ProductService) {}
 
   @Get(':product_id/similar')
-  async similar() {}
+  async similar(
+    @Param('product_id', ParseIntPipe) product_id: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('limit', new DefaultValuePipe(20), ParseIntPipe) limit: number,
+  ) {
+    return await this.productService.similar(product_id, page, limit);
+  }
 
   @Get('special-offers')
   async specialOffers() {}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,8 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
   constructor(private prisma: PrismaService) {}
 
+  async similar(product_id: number, page: number, limit: number) {
+    const product = await this.prisma.product.findFirst({
+      where: {
+        id: product_id,
+      },
+    });
+    if (!product) {
+      throw new NotFoundException('product not found');
+    }
+    const [products, total] = await this.prisma.$transaction([
+      this.prisma.product.findMany({
+        where: {
+          id: {
+            not: product.id,
+          },
+          category_id: product.category_id,
+          brand_id: product.brand_id,
+        },
+        orderBy: {
+          id: 'desc',
+        },
+        skip: (page - 1) * limit,
+        take: limit,
+        include: {
+          productImages: true,
+          offers: {
+            include: {
+              badges: true,
+            },
+            orderBy: {
+              price: 'asc',
+            },
+            take: 1,
+          },
+        },
+      }),
+      this.prisma.product.count({
+        where: {
+          id: {
+            not: product.id,
+          },
+        },
+      }),
+    ]);
+    return {
+      data: product,
+      pagination: {
+        page: page,
+        limit: limit,
+        total,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
+  }
 }
