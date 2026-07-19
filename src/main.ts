9cf39739298731242801c5ef4f83309c07459394
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-07-19 11:10:05 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   app.enableCors({
     origin: '*',
   });
