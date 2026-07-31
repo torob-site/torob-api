ea6ac1520f28fdd39a2079d38adc6d1e514bd619
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-07-31 16:10:54 +0330

    feat: add optinal jwt guard

---
+++ src/auth/optional-jwt.guard.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class OptionalJwtGuard extends AuthGuard('jwt') {
+  handleRequest(err, user) {
+    
+    return user ?? null;
+  }
+}
