8f3f8098849e9662d7a213831d498a1de834a605
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-08-26 20:14:36 +0330

    fix: remove folder online-shop and offline-shop

--- src/panel/online-shop/online-shop.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { OnlineShopService } from './online-shop.service';
-
-@Controller('panel/online-shop')
-export class OnlineShopController {
-  constructor(private onlineShopService: OnlineShopService) {}
-}
